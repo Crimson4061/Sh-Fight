8993b221b6e30fe75d2ea090496623dc64d17c0d
Crimson4061 <58094301+Crimson4061@users.noreply.github.com>
2020-09-10 16:48:44 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,16 @@
+var game = {
+    playerStats = {
+        playerName = "Click to set name",
+        playerHealthBase = 10,
+        playerHeathCurrent = playerHealthBase,
+        playerAttackBase = 10,
+        playerAttackCurrent = playerAttackBase,
+        playerSpeedBase = 10,
+        playerSpeedCurrent = playerSpeedBase,
+        playerIntBase = 10,
+        playerIntCurrent = playerIntBase
+        },
+    enemyStats = {
+        
+    }
+}
